1fab3c8d2a6a2cb144d77d78f5174b207cc68ec0
Penguin-Island <penguinisland.dev@gmail.com>
2021-12-26 16:46:28 +0900

    水族館JSON追加
    Close #1

--- area/kyushu.php
+++ area/kyushu.php
@@ -1,64 +1,46 @@
 <!DOCTYPE html>
 <html>
-    <head>
-		  <meta charset="UTF-8">
-		  <link rel="stylesheet" href="../css/style.css">
-      <title>九州・沖縄エリアの水族館【すいログ】</title>
-    </head>
-    <body>
-      <?php require __DIR__ . '/../header.php'; ?>
 
-      <div class="wave">
-      <div class="container">
-      <h2>福岡</h2>
-      <div class="basho">
-        <h3>マリンワールド海の中道</h3>
-        <p></p>
-        <p>HP：<a href="https://marine-world.jp/"> https://marine-world.jp/</a></p>
-        <p>所在地：〒811-0321 福岡県福岡市東区大字西戸崎18-28</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.8547110378627!2d130.3613595144655!3d33.6609262956987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35418db11a488b47%3A0xbf3a0dd967c4c244!2z44Oe44Oq44Oz44Ov44O844Or44OJ5rW344Gu5Lit6YGT!5e0!3m2!1sja!2sjp!4v1629708474850!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-      </div>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/style.css">
+  <title>九州・沖縄エリアの水族館【すいログ】</title>
+</head>
 
-      <h2>大分</h2>
-      <div class="basho">
-        <h3>大分マリーンパレス水族館「うみたまご」</h3>
-        <p></p>
-        <p>HP：<a href="https://www.umitamago.jp/">https://www.umitamago.jp/</a>
-        <p>所在地：〒870-0802 大分県大分市大字神崎字ウト3078-22</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3336.290838115878!2d131.53341741445342!3d33.258877466568094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3546a139253d6ab5%3A0x39c434d349a8835a!2z5aSn5YiG44Oe44Oq44O844Oz44OR44Os44K55rC05peP6aSo44CM44GG44G_44Gf44G-44GU44CN!5e0!3m2!1sja!2sjp!4v1629708642315!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-      </div>
+<body>
+  <?php require __DIR__ . '/../header.php'; ?>
+  <?php
+  $json = file_get_contents(__DIR__ . '/area.json');
+  $arr = json_decode($json, true); ?>
 
-      <h2>佐賀</h2>
+  <div class="wave">
+    <div class="container">
+      <?php
+      foreach ($arr as $data) {
+      ?>
+        <h2><?= $data['prefecture'] ?></h2>
+        <?php
+        foreach ($data['aquariums'] as $detail) {
+        ?>
+          <div class="basho">
+            <h3><?= $detail['name'] ?></h3>
+            <p></p>
+            <p>HP：<a href="<?= $detail['url'] ?>"> <?= $detail['url'] ?></a></p>
+            <p>所在地：<?= $detail['address'] ?></p>
+            <iframe src="<?= $detail['map'] ?>" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+        <?php
+        }
+        ?>
+      <?php
+      }
+      ?>
+    </div>
+  </div>
 
-      <h2>長崎</h2>
-      <div class="basho">
-        <h3>長崎ペンギン水族館</h3>
-        <p></p>
-        <p>HP：<a href="https://penguin-aqua.jp/">https://penguin-aqua.jp/</a></p>
-        <p>所在地：〒851-0121 長崎県長崎市宿町3-16</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.2854140330114!2d129.9445290144385!3d32.758160492255186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356aaae694236b93%3A0x75f0c01ba16c2e3e!2z6ZW35bSO44Oa44Oz44Ku44Oz5rC05peP6aSo!5e0!3m2!1sja!2sjp!4v1629708844386!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-      </div>
+  <?php require __DIR__ . '/../footer.php'; ?>
+</body>
 
-
-      <h2>熊本</h2>
-      <h2>宮崎</h2>
-
-      <h2>鹿児島</h2>
-      <div class="basho">
-        <h3>いおワールド かごしま水族館</h3>
-        <p></p>
-        <p>HP：<a href="http://ioworld.jp/">http://ioworld.jp/</a></p>
-        <p>所在地：〒892-0814 鹿児島県鹿児島市本港新町3-1</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3398.379442141423!2d130.56253201440484!3d31.59606105056356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x353e5e1abd09df4f%3A0x58220efc5a3b0ba0!2z44GE44GK44Ov44O844Or44OJ44GL44GU44GX44G-5rC05peP6aSo!5e0!3m2!1sja!2sjp!4v1629708879457!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-      </div>
-
-      <h2>沖縄</h2>
-
-        </div>
-        </div>
-        
-        <?php require __DIR__ . '/../footer.php'; ?>
-    </body>
 </html>
 
 <!-- <div class="basho">

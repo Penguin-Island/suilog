f8cbc28a4cd65b92208ba66fb9585ec753650a9a
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-02 22:30:05 +0900

    ログイン状態のチェック機能追加（DB未接続）

--- input.php
+++ input.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+
+if (isset($$_SESSION['id']) && ($_SESSION['time'] + 3600) > time()) {
+	//ログインしている
+	$_SESSION['time'] = time();
+
+	$stmt = $db->prepare('SELECT * FROM users WHERE id=?', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+	$stmt->bindValue(1, $_SESSION['id']);
+	$stmt->execute();
+	$result = $stmt->fetch();
+} else {
+	//ログインしていない
+	header('Location: login.php');
+	exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 

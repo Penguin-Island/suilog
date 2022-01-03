deb72bf84f96844055a580f2d78d0b0e1d335b5d
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-03 17:47:21 +0900

    ログアウト機能実装

--- header.php
+++ header.php
@@ -1,9 +1,18 @@
+<?php
+session_start();
+if( !empty($_GET['btn_logout']) ) {
+	unset($_SESSION['id']);
+    header('Location: login.php');
+	exit;
+}
+?>
+
 <header>
     <h1 class="title"><a href="/suilog/" class="title-a">すいログ</a></h1>
     <nav class="nav">
         <ul class="menu-group">
             <li class="menu-item"><a href="/suilog/" class="menu-item-a">Top</a></li>
-            <li class="menu-item"><a href="/suilog/" class="menu-item-a">さがす</a></li>
+            <li class="menu-item"><a href="/suilog/#srch" class="menu-item-a">さがす</a></li>
             <li class="menu-item"><a href="/suilog/#review" class="menu-item-a">おもいで</a></li>
         </ul>
     </nav>
@@ -16,5 +25,8 @@
     <ul class="login-group">
         <li class="login-item"><a href="/suilog/login.php" class="login-item-a">ログイン</a></li>
         <li class="login-item"><a href="/suilog/regist.php" class="login-item-a">新規登録</a></li>
+        <li class="login-item"><form method="get" action="">
+    <input type="submit" name="btn_logout" value="ログアウト">
+</form></li>
     </ul>
 </header>

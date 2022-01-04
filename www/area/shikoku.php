8d195ffb3a684185e9c0f9d847ac9108730fc7fc
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-04 16:14:35 +0900

    Add files to area/

---
+++ www/area/shikoku.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>四国エリアの水族館【すいログ】</title>
+</head>
+
+<body>
+    <?php require __DIR__ . '/../header.php'; ?>
+
+    <div class="wave">
+        <div class="back">
+            <div class="container" style="height: 500px;">
+                <div class="spacer"></div>
+                <p style="text-align: center">
+                    準備中
+                    （九州・沖縄エリアのページをご利用いただけます）
+                </p>
+            </div>
+        </div>
+    </div>
+    <?php require __DIR__ . '/../footer.php'; ?>
+</body>
+
+</html>

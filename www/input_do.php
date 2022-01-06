7d6d456fd152d9487df7df95965e35cfdcec832c
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-06 19:28:55 +0900

    Clean

--- www/input_do.php
+++ www/input_do.php
@@ -1,19 +1,17 @@
 <?php
 session_start();
-require ( __DIR__ . '/dbconnect.php');
-//input.php からのフォームを受け取る
+require(__DIR__ . '/dbconnect.php');
 
-//POSTの中身を確認する
 if (!isset($_POST['name'], $_POST['memo'])) {
     http_response_code(400);
     exit();
 }
-//var_dump($_POST);
 header('Location: input.php');
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
 </head>
@@ -21,7 +19,7 @@ header('Location: input.php');
 <body>
     <pre>
     <?php
-	$stmt = $db->prepare('INSERT INTO places (userid, place, date, review, created_at) VALUES (:userid, :place, :date, :review, NOW())');
+    $stmt = $db->prepare('INSERT INTO places (userid, place, date, review, created_at) VALUES (:userid, :place, :date, :review, NOW())');
     $stmt->bindValue(':userid', $_SESSION['id']);
     $stmt->bindValue(':place', $_POST['name']);
     $stmt->bindValue(':date', $_POST['date']);

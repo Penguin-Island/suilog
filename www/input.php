32925eadd254b3dfc793fced5eda1caffb9960c8
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-30 21:24:15 +0900

    Use  check.php

--- www/input.php
+++ www/input.php
@@ -1,20 +1,6 @@
 <?php
 session_start();
-require(__DIR__ . '/dbconnect.php');
-
-if (isset($_SESSION['id']) && ($_SESSION['time'] + 3600) > time()) {
-	//ログインしている
-	$_SESSION['time'] = time();
-
-	$stmt = $db->prepare('SELECT * FROM users WHERE id=?', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
-	$stmt->bindValue(1, $_SESSION['id']);
-	$stmt->execute();
-	$result = $stmt->fetch();
-} else {
-	//ログインしていない
-	header('Location: login.php');
-	exit;
-}
+require(__DIR__ . '/check.php');
 ?>
 <!DOCTYPE html>
 <html>

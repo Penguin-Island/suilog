28aab27a536817396a7e7c232e799ea234c84e1f
Penguin-Island <penguinisland.dev@gmail.com>
2022-01-31 21:27:11 +0900

    Change UI

--- www/view.php
+++ www/view.php
@@ -18,7 +18,7 @@ require(__DIR__ . '/check.php');
     <div class="container">
       <div class="spacer"></div>
 
-      <h2>思い出をみる</h2>
+      <h2>おもいでをふりかえる</h2>
 
       <?php
       $cnt = $db->prepare('SELECT COUNT(*) AS record_count FROM places WHERE userid=?');
@@ -26,34 +26,27 @@ require(__DIR__ . '/check.php');
       $cnt->execute();
       $cntresult = $cnt->fetch();
       ?>
-      
+
       <article>
-        <h4>思い出 <?= $cntresult['record_count'] ?>つ</h4>
+        <h4>おもいで <?= $cntresult['record_count'] ?>つ</h4>
       </article>
 
-      <div class="grad-wrap">
-        <input id="trigger1" class="grad-trigger" type="checkbox">
-        <label class="grad-btn" for="trigger1"></label>
-        <div class="grad-item">
-          <div class="all-form">
-            <?php
-            $plc = $db->prepare('SELECT * FROM places WHERE userid=? ORDER BY id DESC');
-            $plc->bindValue(1, $_SESSION['id']);
-            $plc->execute();
-            ?>
+      <div class="all-form">
+        <?php
+        $plc = $db->prepare('SELECT * FROM places WHERE userid=? ORDER BY id DESC');
+        $plc->bindValue(1, $_SESSION['id']);
+        $plc->execute();
+        ?>
 
-            <article>
-              <?php while ($plcresult = $plc->fetch()) : ?>
-                <p><?= $plcresult['place'] ?></p>
-                <p><?= $plcresult['review'] ?></p>
-                <p><?= $plcresult['created_at'] ?></p>
-                <hr>
-              <?php endwhile; ?>
-            </article>
-          </div>
-        </div>
+        <article>
+          <?php while ($plcresult = $plc->fetch()) : ?>
+            <p><?= $plcresult['place'] ?></p>
+            <p><?= $plcresult['review'] ?></p>
+            <p><?= $plcresult['created_at'] ?></p>
+            <hr>
+          <?php endwhile; ?>
+        </article>
       </div>
-
     </div>
   </div>
 
